8680da20c0b0bec465d3fd4500045335a6c36c75
halata83 <hali.lucky@gmail.com>
2024-02-23 08:24:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-MODEL_URL=$(bashio::config "MODEL_URL")
-LABELS_URL=$(bashio::config "LABELS_URL")
-
+#MODEL_URL="https://raw.githubusercontent.com/google-coral/edgetpu/master/test_data/ssd_mobilenet_v2_coco_quant_postprocess.tflite"
+MODEL_URL="https://raw.githubusercontent.com/google-coral/test_data/master/efficientdet_lite3_512_ptq_edgetpu.tflite"
+LABELS_URL="https://github.com/google-coral/edgetpu/raw/master/test_data/coco_labels.txt"
+LOG=$true
+echo $MODEL_URL
+echo $LABELS_URL
 MODEL_FILE=`echo ${MODEL_URL} | sed 's:.*/::'`
 LABELS_FILE=`echo ${LABELS_URL} | sed 's:.*/::'`
+echo $MODEL_FILE
+echo $LABELS_FILE
 
+mkdir -p /config
 rm -f /app/coral.log
-if bashio::config.true "ACCESS_LOG"; then
+if $LOG; then
   touch /config/coral_access.log
   ln -s /config/coral_access.log /app/coral.log
   echo "Logging to /config/coral_access.log"
@@ -26,3 +32,4 @@ cd /app
 
 echo "Starting the server..."
 exec python3 /app/coral-app.py --model  "${MODEL_FILE}" --labels "${LABELS_FILE}" --models_directory "/app/models/"
+
